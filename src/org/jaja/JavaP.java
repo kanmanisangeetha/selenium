313522ed99750ec92a0660bb9720fa8103a13c71
kanmanisangeetha <nkanmanisangeetha@gmail.com>
2023-01-17 07:47:18 +0530

    message

---
+++ src/org/jaja/JavaP.java
@@ -0,0 +1,26 @@
+package org.jaja;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class JavaP {
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.geccko.driver", "C:\\Users\\DELL\\eclipse-workspace\\SeliniumFromAlertTask\\driver\\chromedriver.exe");
+		WebDriver driver=new FirefoxDriver();
+		driver.get("https://www.chairish.com/");
+		WebElement search=driver.findElement(By.name("q"));
+		search.sendKeys("FlowerLamp");
+		WebElement submit=driver.findElement(By.xpath("//span[@class='js-submit-icon cicon cicon-search-fill']"));
+		submit.click();
+		
+		
+		
+}
+}
+
+
+
+
